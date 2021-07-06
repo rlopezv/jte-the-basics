281ee42e9bab82c95671c4ce3c1167cd3f850caa
ramon.lopezviana@altran.com <ramon.lopezviana@altran.com>
2021-07-06 12:05:49 +0200

    back to basics

--- libraries/maven/steps/build.groovy
+++ libraries/maven/steps/build.groovy
@@ -1,33 +1,5 @@
-package libraries.maven
-
 void call(app_env){
     stage("Maven: Build"){
-        withMaven(maven: config.mavenId) {
-            String command = "mvn "
-            if (!phases) {
-                error "Must supply phase for Maven"
-            }
-            phases.each { phase -> command += "${phase} "}
-
-            if (goals) {
-                goals.each { goal -> command += "${goal} " }
-            }
-
-            if (properties) {
-                properties.each { propertyName, value -> command += "-D${propertyName} "
-                    if (value != null) {
-                        command += "= ${value} "
-                    }
-                }
-            }
-
-            if (profiles) {
-                command += "-P"
-                String joined = profiles.join(",")
-                command += joined
-            }
-
-            sh command
-        }
+        println "build from the maven library parent"
     }
 }
