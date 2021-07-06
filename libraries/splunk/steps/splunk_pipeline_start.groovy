c9f5fc9a172107dbabf9406879644cbf4eeff8b4
ramon.lopezviana@altran.com <ramon.lopezviana@altran.com>
2021-07-06 08:45:37 +0200

    aop

--- libraries/splunk/steps/splunk_pipeline_start.groovy
+++ libraries/splunk/steps/splunk_pipeline_start.groovy
@@ -2,3 +2,13 @@
 void call(){
     println "Splunk: beginning of the pipeline!"
 }
+
+@BeforeStep
+void before(){
+    println "Splunk: running before the ${hookContext.library} library's ${hookContext.step} step"
+}
+
+@AfterStep
+void after(){
+    println "Splunk: running after the ${hookContext.library} library's ${hookContext.step} step"
+}
