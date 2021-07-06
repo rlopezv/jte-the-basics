cdb4a08bb7e61e2eaee4bed4cab41702dfb1d5f9
ramon.lopezviana@altran.com <ramon.lopezviana@altran.com>
2021-07-06 08:48:34 +0200

    hookcontext

--- libraries/splunk/steps/splunk_pipeline_start.groovy
+++ libraries/splunk/steps/splunk_pipeline_start.groovy
@@ -5,10 +5,16 @@ void call(){
 
 @BeforeStep
 void before(){
+    println "________________"
+    println "${hookContext}"
+    println "________________"
     println "Splunk: running before the ${hookContext.library} library's ${hookContext.step} step"
 }
 
 @AfterStep
 void after(){
+    println "________________"
+    println "${hookContext}"
+    println "________________"
     println "Splunk: running after the ${hookContext.library} library's ${hookContext.step} step"
 }
