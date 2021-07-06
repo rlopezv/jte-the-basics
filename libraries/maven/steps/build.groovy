20ce71d84f8578c21e76f45c3e0970b5fe63b3cb
ramon.lopezviana@altran.com <ramon.lopezviana@altran.com>
2021-07-06 11:32:31 +0200

    Mvn

--- libraries/maven/steps/build.groovy
+++ libraries/maven/steps/build.groovy
@@ -1,5 +1,10 @@
 void call(app_env){
     stage("Maven: Build"){
+        node {
+        withMaven (maven: 'm3') {
+            sh "mvn clean verify"
+        }
+        }
         println app_env
         println "build from the maven library"
     }
