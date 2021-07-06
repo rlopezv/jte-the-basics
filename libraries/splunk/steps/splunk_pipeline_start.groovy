fc5ad94d403ab9adc964ff5ec02675fbc1694cd2
ramon.lopezviana@altran.com <ramon.lopezviana@altran.com>
2021-07-06 09:20:41 +0200

    remove step

--- libraries/splunk/steps/splunk_pipeline_start.groovy
+++ libraries/splunk/steps/splunk_pipeline_start.groovy
@@ -2,19 +2,3 @@
 void call(){
     println "Splunk: beginning of the pipeline!"
 }
-
-@BeforeStep
-void before(){
-    println "________________"
-    println "${hookContext}"
-    println "________________"
-    println "Splunk: running before the ${hookContext.library} library's ${hookContext.step} step"
-}
-
-@AfterStep
-void after(){
-    println "________________"
-    println "${hookContext}"
-    println "________________"
-    println "Splunk: running after the ${hookContext.library} library's ${hookContext.step} step"
-}
