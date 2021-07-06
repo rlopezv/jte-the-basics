b272e0847459c4bb36da608c41ce6b562d22bbe5
ramon.lopezviana@altran.com <ramon.lopezviana@altran.com>
2021-07-06 11:51:43 +0200

    SDP

--- libraries/maven/steps/build.groovy
+++ libraries/maven/steps/build.groovy
@@ -1,5 +1,31 @@
 void call(app_env){
     stage("Maven: Build"){
-        println "build from the maven library parent"
+        withMaven(maven: config.mavenId) {
+            String command = "mvn "
+            if (!phases) {
+                error "Must supply phase for Maven"
+            }
+            phases.each { phase -> command += "${phase} "}
+
+            if (goals) {
+                goals.each { goal -> command += "${goal} " }
+            }
+
+            if (properties) {
+                properties.each { propertyName, value -> command += "-D${propertyName} "
+                    if (value != null) {
+                        command += "= ${value} "
+                    }
+                }
+            }
+
+            if (profiles) {
+                command += "-P"
+                String joined = profiles.join(",")
+                command += joined
+            }
+
+            sh command
+        }
     }
 }
