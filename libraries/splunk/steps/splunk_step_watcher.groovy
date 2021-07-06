fcf20928c55ea390021bedb3ab8bdc66a9d6a1f4
ramon.lopezviana@altran.com <ramon.lopezviana@altran.com>
2021-07-06 09:44:15 +0200

    context?????

--- libraries/splunk/steps/splunk_step_watcher.groovy
+++ libraries/splunk/steps/splunk_step_watcher.groovy
@@ -3,14 +3,16 @@ void before(context){
   println "Splunk: running before the ${context.library} library's ${context.step} step" 
 }
 
-@AfterStep({ config.afterSteps ? (context.step in config.afterSteps) : true })
+@AfterStep
+//({ config.afterSteps ? (context.step in config.afterSteps) : true })
 void after(context){
   //println "Splunk: running after the ${context.library} library's ${context.step} step" 
-  println "_________________"
+  println "_______${context}__________"
 }
 
-@AfterStep({ currentBuild.result.toString() == "FAILURE" })
+@AfterStep
+//({ currentBuild.result.toString() == "FAILURE" })
 void afterFailure(context){
   //println "Splunk: running after the ${context.library} library's ${context.step} step failure" 
-  println "_________________"
+  println "_______${context}__________"
 }
