ed97519d5bd20d57e652a51d5dc177a640e9f3ed
ramon.lopezviana@altran.com <ramon.lopezviana@altran.com>
2021-07-06 11:38:42 +0200

    Error notacion

--- libraries/maven/steps/build.groovy
+++ libraries/maven/steps/build.groovy
@@ -1,9 +1,7 @@
 void call(app_env){
     stage("Maven: Build"){
-        node {
         withMaven (maven: 'm3') {
             sh "mvn clean verify"
-        }
         }
         println app_env
         println "build from the maven library"
