94d28bb2648dae1cfee9b59a0e3fa8585407d211
ramon.lopezviana@altran.com <ramon.lopezviana@altran.com>
2021-07-06 09:31:44 +0200

    error contexto?

--- libraries/splunk/steps/splunk_step_watcher.groovy
+++ libraries/splunk/steps/splunk_step_watcher.groovy
@@ -5,10 +5,12 @@ void before(context){
 
 @AfterStep({ config.afterSteps ? (context.step in config.afterSteps) : true })
 void after(context){
-  println "Splunk: running after the ${context.library} library's ${context.step} step" 
+  //println "Splunk: running after the ${context.library} library's ${context.step} step" 
+  println "_________________"
 }
 
 @AfterStep({ currentBuild.result.toString() == "FAILURE" })
 void afterFailure(context){
-  println "Splunk: running after the ${context.library} library's ${context.step} step failure"  
+  //println "Splunk: running after the ${context.library} library's ${context.step} step failure" 
+  println "_________________"
 }
