dd040f21efb45fa5859634aaf2c87bd240b8b917
Ramon Lopez Viana <44174626+rlopezv@users.noreply.github.com>
2021-07-08 13:49:24 +0200

    Update build.groovy

--- libraries/maven/steps/build.groovy
+++ libraries/maven/steps/build.groovy
@@ -1,6 +1,6 @@
 void call(app_env){
-    node {
     stage("Maven: Build"){
+        node {
         println "Step Configuration: ${config}"
         println "build from the maven library tenant"
         withMaven {
