9944f7443bd2ca1a2cd75423e91808a03324db4e
ramon.lopezviana@altran.com <ramon.lopezviana@altran.com>
2021-07-08 11:41:16 +0200

    imports

--- libraries/config/steps/init.groovy
+++ libraries/config/steps/init.groovy
@@ -1,8 +1,14 @@
+import groovy.json.JsonOutput
+
 @Init
 void call(){
     node {
         sh 'java -version'
     }
+    
+    conf = JsonOutput.prettyPrint(JsonOutput.toJson(dev))
+
+    println "echo ${conf}"
 
     println "echo ${dev.long_name}"
     println "Beginning of the pipeline!"
