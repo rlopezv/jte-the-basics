cfec05d4d148574d42e9a6e6d45736a7cdfaf7c4
ramon.lopezviana@altran.com <ramon.lopezviana@altran.com>
2021-07-09 08:48:55 +0200

    remvoe gradle

--- libraries/maven/steps/build.groovy
+++ libraries/maven/steps/build.groovy
@@ -1,5 +1,13 @@
 void call(app_env){
     stage("Maven: Build"){
         println "build from the maven library parent"
+        node {
+        withMaven {
+            sh "mvn -v"
+        }
+ //withGradle {
+ //   sh 'gradle -v'
+ // }
+        }
     }
 }
